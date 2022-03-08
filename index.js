5216d8aa7569ce20e6b96e4c21b747fa22d982d9
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-03-08 15:22:46 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 //step 1 creazione del pulsante
-var btn = document.createElement('button');
+const btn = document.createElement('button');
 btn.innerHTML = 'P';
 document.body.appendChild(btn);
 
 //step 2 aggiunta di un paragrafo dinamico con valore da variabile
-let parValue = 'paragrafo dinamico!';
-
-parView = () => {
-  let p1 = document.createElement('p');
-  p1.innerHTML = parValue;
-  document.body.appendChild(p1);
-};
+const parValue = 'paragrafo dinamico!';
+const p1 = document.createElement('p');
+document.body.appendChild(p1);
+parView = () => (p1.innerHTML = parValue);
 
 btn.addEventListener('click', parView);
