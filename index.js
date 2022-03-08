418dc5930d884e339ed5d26b13b6f26f31ea1a33
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-03-08 17:12:42 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //step 1 creazione del pulsante
-const btn = document.createElement('button');
+/*const btn = document.createElement('button');
 btn.innerHTML = 'P';
 document.body.appendChild(btn);
 
@@ -9,22 +9,26 @@ const p1 = document.createElement('p');
 document.body.appendChild(p1);
 
 btn.addEventListener('click', () => (p1.innerHTML = parValue));
+*/
 
 //step 3 array di 10 tasti, ognuno visualizza il proprio valore
 
-let butArray = new Array(10);
-butArray[0] = 'mele';
-butArray[1] = 'pere';
-butArray[2] = 'susine';
-butArray.forEach(creaPar);
+let butnArray = new Array(10);
+butnArray[0] = 'Dylan';
+butnArray[1] = undefined;
+butnArray[2] = 'Bloch';
+butnArray[3] = undefined;
+butnArray[4] = 'Groucho';
+butnArray.forEach(creaPar);
 
 function creaPar(value, index) {
   let vbtn = document.createElement('button');
   vbtn.innerHTML = 'P' + index;
+  vbtn.value = value != undefined ? value : '';
   document.body.appendChild(vbtn);
-  vbtn.addEventListener('click', showText(this.value));
+  vbtn.addEventListener('click', function () {
+    p1.innerHTML = this.value;
+  });
 }
-
-function showText(value) {
-  p1.innerHTML = value;
-}
+const p1 = document.createElement('p');
+document.body.appendChild(p1);
