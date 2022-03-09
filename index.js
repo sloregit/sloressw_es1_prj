74e38b0321d0cca793fcd79e9f9f5f2af7511f27
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-03-09 20:45:51 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
 //step 0 creazione del pulsante
-/*const btn = document.createElement('button');
+const btn = document.createElement('button');
 btn.innerHTML = 'P';
 document.body.appendChild(btn);
 
 //step 1 aggiunta di un paragrafo con valore visualizzato al click
 const parValue = 'paragrafo dinamico!';
-const p1 = document.createElement('p');
+const p1 = document.getElementById('p1');
 document.body.appendChild(p1);
 
 btn.addEventListener('click', () => (p1.innerHTML = parValue));
-*/
 
-//step 3 array di 10 tasti, ognuno visualizza il proprio valore
-
-let butnArray = new Array(10);
-butnArray[0] = 'Dylan';
-butnArray[1] = undefined;
-butnArray[2] = 'Bloch';
-butnArray[3] = undefined;
-butnArray[4] = 'Groucho';
-butnArray.forEach(creaPar);
-
-function creaPar(value, index) {
-  let vbtn = document.createElement('button');
-  vbtn.innerHTML = 'P' + index;
-  vbtn.value = value != undefined ? value : '';
-  document.body.appendChild(vbtn);
-  vbtn.addEventListener('click', function () {
-    p1.innerHTML = this.value;
-  });
-}
-const p1 = document.createElement('p');
+const p2 = document.createElement('p');
 document.body.appendChild(p1);
