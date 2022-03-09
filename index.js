702d67a854a8d5e9928054f09c41c526c49d8ddd
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-03-09 20:51:27 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//step 0 creazione del pulsante
+//step 0 creazione del pulsante con etichetta
 const btn = document.createElement('button');
 btn.innerHTML = 'P';
 
 //step 1 aggiunta di un paragrafo
-const parValue = 'paragrafo dinamico!';
 const p1 = document.getElementById('p1');
 p1.appendChild(btn);
 
+//step 2 aggiunto paragrafo con valore visualizzato al click
 const p2 = document.createElement('p2');
-document.body.appendChild(p1);
+document.body.appendChild(p2);
+const parValue = 'paragrafo dinamico!';
 
 btn.addEventListener('click', () => (p2.innerHTML = parValue));
