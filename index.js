e9303acd154b84ad4ce3032a257211c041937492
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-03-10 00:01:01 +0100

    1 file modified

--- index.js
+++ index.js
@@ -10,35 +10,26 @@ const parNomi = document.getElementById('parNomi');
 function addBtn(value, index) {
   let showNome = document.createElement('button');
   let aCapo = document.createElement('br');
-  showNome.innerHTML = 'P' + (index + 1);
+  //this * 10 per avere il numero del posto
+  showNome.innerHTML = 'P' + (index + 1 + this * 10);
   showNome.value = value != undefined ? value : ''; // x sicurezza
   parPosti.appendChild(showNome);
-  parPosti.appendChild(aCapo);
+  index >= 9 ? parPosti.appendChild(aCapo) : '';
+  //
+
   showNome.addEventListener('click', function () {
     parNomi.innerHTML = this.value;
   });
 }
 
-//postiArray.forEach(addBtn);
-
-//step 4
-/*
-let prenotazioni = new Array();
-prenotazioni = postiArray.map(creaB);
-
-function creaB(value, index) {
-  prenotazioni[index] = value;
-}
-*/
-
 //creo le file e le inserisco nella platea
 const file = new Array(10).fill('x');
-
-const platea = new Array(10);
-platea.map(creaFile());
+const platea = new Array(10).fill('');
 
 function creaFile(fila, index) {
-  platea[index] = file.map(addBtn);
-  console.log(file);
+  //index = this. nella callback
+  return (platea[index] = file.map(addBtn, index));
 }
+platea.map(creaFile);
+platea[0][7] = 'Chiarbi';
 console.log(platea);
