44fc385c1fbcba1e9156ae3f9f193e122ac22059
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-03-09 20:42:38 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//step 1 creazione del pulsante
+//step 0 creazione del pulsante
 /*const btn = document.createElement('button');
 btn.innerHTML = 'P';
 document.body.appendChild(btn);
 
-//step 2 aggiunta di un paragrafo con valore visualizzato al click
+//step 1 aggiunta di un paragrafo con valore visualizzato al click
 const parValue = 'paragrafo dinamico!';
 const p1 = document.createElement('p');
 document.body.appendChild(p1);
